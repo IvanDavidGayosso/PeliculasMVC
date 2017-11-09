c29bead8ea137a52d4e0b0c0f5c765d6444cacf0
IvanDavidGayosso <31744431+IvanDavidGayosso@users.noreply.github.com>
2017-11-09 07:17:16 -0600

    Vistas corregidas !!

--- PeliculasMVC/src/views/ViewPrincipal.java
+++ PeliculasMVC/src/views/ViewPrincipal.java
@@ -56,11 +56,11 @@ public class ViewPrincipal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 574, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 279, Short.MAX_VALUE)
+            .addGap(0, 386, Short.MAX_VALUE)
         );
 
         pack();
